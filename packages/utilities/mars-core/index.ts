5ac10f71cc4ab70e29566015ec94a1aba373dc38
MarsGotta <marcelagotta@gmail.com>
2022-06-09 00:43:46 +0200

    chore: core fix changed

--- packages/utilities/mars-core/index.ts
+++ packages/utilities/mars-core/index.ts
@@ -7,10 +7,10 @@ export {
   unsafeCSS,
 } from 'lit-element';
 export { render } from 'lit-html';
-export { classMap } from 'lit-html/directives/class-map';
-export { styleMap } from 'lit-html/directives/style-map';
+export { classMap } from 'lit-html/directives/class-map.js';
+export { styleMap } from 'lit-html/directives/style-map.js';
 export { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
-export { ifDefined } from 'lit-html/directives/if-defined';
+export { ifDefined } from 'lit-html/directives/if-defined.js';
 
 export * from './styles/fundations.js';
 export { tagsFont } from './styles/tags.js';
